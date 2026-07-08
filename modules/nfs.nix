e6049a53b5b619aa2170fc63487f2cbf328f797d
Ell Ari Nitsche <mail@ellanitsche.de>
2026-07-08 19:57:36 +0200

    removed ids

--- modules/nfs.nix
+++ modules/nfs.nix
@@ -4,8 +4,8 @@
         enable = true;
         exports = ''
         /srv/nfs                    192.168.0.0/24(rw,fsid=0,no_subtree_check)
-        /srv/nfs/ruth               192.168.0.0/24(rw,sync,nohide,insecure,no_subtree_check,all_squash,anonuid=65534,anongid=65534)
-        /srv/nfs/ell                192.168.0.0/24(rw,sync,nohide,insecure,no_subtree_check,all_squash,anonuid=65534,anongid=65534)
+        /srv/nfs/ruth               192.168.0.0/24(rw,sync,nohide,insecure,no_subtree_check,all_squash)
+        /srv/nfs/ell                192.168.0.0/24(rw,sync,nohide,insecure,no_subtree_check,all_squash)
         '';
     };
 
