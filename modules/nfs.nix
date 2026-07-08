1c559f90baf8b6942796f1fe9db43b93e93ca430
Ell Ari Nitsche <mail@ellanitsche.de>
2026-07-08 23:23:08 +0200

    Permit localhost

--- modules/nfs.nix
+++ modules/nfs.nix
@@ -6,6 +6,7 @@
         /srv/nfs                    192.168.0.0/24(rw,fsid=0,no_subtree_check)
         /srv/nfs/ruth               192.168.0.0/24(rw,sync,nohide,insecure,no_subtree_check,all_squash)
         /srv/nfs/ell                192.168.0.0/24(rw,sync,nohide,insecure,no_subtree_check,all_squash)
+        /srv/nfs/ell                127.0.0.0/8(rw,sync,nohide,insecure,no_subtree_check,all_squash)
         '';
     };
 
