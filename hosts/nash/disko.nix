7e2881154d1feed8c61445a69f7100b0eb7fc67e
Ell Ari Nitsche <mail@ellanitsche.de>
2026-07-08 13:42:24 +0200

    change

--- hosts/nash/disko.nix
+++ hosts/nash/disko.nix
@@ -85,16 +85,16 @@
           size = "98%";
           lvm_type = "thin-pool";
         };
-        # ell_lv = {
-        #   size = "200G";
-        #   lvm_type = "thinlv";
-        #   pool = "thinpool";
-        #   content = {
-        #     type = "filesystem";
-        #     format = "btrfs";
-        #     mountpoint = "/mnt/nfs/ell";
-        #   };
-        # };
+        ell_lv = {
+          size = "200G";
+          lvm_type = "thinlv";
+          pool = "thinpool";
+          content = {
+            type = "filesystem";
+            format = "btrfs";
+            mountpoint = "/mnt/nfs/ell";
+          };
+        };
 
         # ruth_lv = {
         #   size = "200G";
