c629d9866d3946a51cadfe62b7a15e671b6c18a5
Ell Ari Nitsche <mail@ellanitsche.de>
2026-07-08 19:47:54 +0200

    fixed shares

--- modules/nfs.nix
+++ modules/nfs.nix
@@ -3,8 +3,9 @@
     services.nfs.server = {
         enable = true;
         exports = ''
-        /srv/nfs                      192.168.0.0/24(rw,fsid=0,no_subtree_check)
-        /srv/nfs/share                192.168.0.0/24(rw,nohide,insecure,no_subtree_check)
+        /srv/nfs                    192.168.0.0/24(rw,fsid=0,no_subtree_check)
+        /srv/nfs/ruth               192.168.0.0/24(rw,nohide,insecure,no_subtree_check)
+        /srv/nfs/ell                192.168.0.0/24(rw,nohide,insecure,no_subtree_check)
         '';
     };
 
